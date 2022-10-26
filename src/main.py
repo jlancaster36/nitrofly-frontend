9da90cd8af68f01fb07bf8e374bae81402431374
Joshua Lancaster <joshua.lancaster@kudelskisecurity.com>
2022-10-26 05:52:35 -0700

    Add settings button

--- src/main.py
+++ src/main.py
@@ -1,30 +1,49 @@
 # Sys is always needed
 import sys
-# QtCore will be needed for some slider properties
-from PyQt5.QtCore import Qt
+from PyQt6.QtCore import Qt
 # QtWidgets includes many typical objects
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,
-                             QHBoxLayout, QVBoxLayout,
+from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton,
+                             QHBoxLayout, QVBoxLayout, QGridLayout,
                              QLineEdit, QLabel, QRadioButton,
-                             QSpinBox, QSlider)
-class MainWindow(QWidget):
+                             QSpinBox, QSlider, QMainWindow,
+                             QMenu)
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('NITROFLY EMULATION FRONTEND')
 
+        # settings = self.addToolBar("Settings")
+        # help = self.addToolBar("Help")
+
+        mainBox = QVBoxLayout()
+        
         settingsBar = QHBoxLayout()
         settingsButton = QPushButton("Settings")
         # settingsButton.setStyleSheet
-        settingsButton.clicked(self.settings())
+        settingsButton.clicked.connect((self.settings))
+        settingsBar.addWidget(settingsButton)
 
-        
+        mainWidget = QWidget()
+        mainWidget.setLayout(settingsBar)
+        self._createMenuBar()
+        self.setCentralWidget(mainWidget)
+
     def settings(self):
         print("Gathering Metadata...")
+
+    def _createMenuBar(self):
+        menuBar = self.menuBar()
+        # Creating menus using a QMenu object
+        fileMenu = QMenu(" &File", self)
+        menuBar.addMenu(fileMenu)
+        # Creating menus using a title
+        editMenu = menuBar.addMenu(" &Edit")
+        helpMenu = menuBar.addMenu(" &Help")
+
         
 
 if __name__ == '__main__':
-    # Always include the following code to initialize your application.
     app = QApplication(sys.argv)
     main = MainWindow()
     main.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
--- src/metadata.py
+++ src/metadata.py
@@ -1,1 +1,2 @@
-def getMetadata()
+def getMetadata():
+    print('');
